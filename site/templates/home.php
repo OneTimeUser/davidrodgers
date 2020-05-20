d584fa497432fbbfdcf11c7af50219ba35162978
aston <jasonaston@gmail.com>
2020-05-19 19:32:19 -0700

    dump

--- site/templates/home.php
+++ site/templates/home.php
@@ -10,112 +10,112 @@
  */
 ?>
 
-    <?php snippet('header') ?>
-    <!-- In this link we call `$site->url()` to create a link back to the homepage -->
+<?php snippet('header') ?>
+<!-- In this link we call `$site->url()` to create a link back to the homepage -->
 
-    <a class="logo" href="<?= $site->url() ?>">D.R</a>
-    <!--       <a class="back" href="<?= $site->url() ?>">BACK TO HOME PAGE</a>-->
+<a class="logo" href="<?= $site->url() ?>">D.R</a>
+<!--       <a class="back" href="<?= $site->url() ?>">BACK TO HOME PAGE</a>-->
 
-    <nav id="menu" class="menu">
-        <div class="address">
-            <!--            <p>DAVID RODGERS INC</p>-->
-            <p>
-                <?= $site->address()->kirbytextinline() ?>
-            </p>
+<nav id="menu" class="menu">
+    <div class="address">
+        <!--            <p>DAVID RODGERS INC</p>-->
+        <p>
+            <?= $site->address()->kirbytextinline() ?>
+        </p>
 
-        </div>
-        <div class="contact">
-            <p>T:
-                <?= $site->phone()->kirbytextinline() ?>
-            </p>
-            <p><a href="mailto:<?= $site->email() ?>"><?= $site->email()->kt() ?></a></p>
-        </div>
-
-    </nav>
-    </header>
-
-    <svg height="0">
-</svg>
-    <div id="splash">
-        <div class="panel middle">
-            <p class="logo">D.R</p>
-            <p>DAVID RODGERS INC</p>
-        </div>
+    </div>
+    <div class="contact">
+        <p>T:
+            <?= $site->phone()->kirbytextinline() ?>
+        </p>
+        <p><a href="mailto:<?= $site->email() ?>"><?= $site->email()->kt() ?></a></p>
     </div>
 
-    <div class="main__content home-page" id="home-page">
+</nav>
+</header>
 
-        <?php 
+<svg height="0">
+</svg>
+<div id="splash">
+    <div class="panel middle">
+        <p class="logo">D.R</p>
+        <p>DAVID RODGERS INC</p>
+    </div>
+</div>
+
+<div class="main__content home-page" id="home-page">
+
+    <?php 
   // we always use an if-statement to check if a page exists to prevent errors 
   // in case the page was deleted or renamed before we call a method like `children()` in this case
      $albums = page('projects')->children()->listed()->filterBy("category", "featured", ",");
 
         ?>
 
-        <section class="proj featured">
-            <ul class="client" data-page="<?= $page->url() ?>">
-                <?php foreach ($albums as $album): ?>
-                <li class="client-name">
+    <section class="proj featured">
+        <ul class="client" data-page="<?= $page->url() ?>">
+            <?php foreach ($albums as $album): ?>
+            <li class="client-name">
 
-                    <div class="client-content">
-                        <article>
-                            <div class='project__container'>
-                                <div class='project__text'>
-                                    <h3 class="client__title">
+                <div class="client-content">
+                    <article>
+                        <div class='project__container'>
+                            <div class='project__text'>
+                                <h3 class="client__title">
 
-                                        <?php foreach ($album->tags()->split() as $tags): ?>
-                                        <span><?= $tags ?></span>
-                                        <?php endforeach ?>
-                                    </h3>
+                                    <?php foreach ($album->tags()->split() as $tags): ?>
+                                    <span><?= $tags ?></span>
+                                    <?php endforeach ?>
+                                </h3>
 
-                                    <span class='project__headline'><?= $album->headline()->value() ?></span>
-                                    <p class='project__sub'>
-                                        <?= $album->subheading()->value() ?>
-                                    </p>
-                                    <p class='project__desc'>
-                                        <?= $album->description()->value() ?>
-                                    </p>
-                                </div>
-                                <div class='project__images main-carousel lazy'>
-                                    <?php if ($album->video()->isNotEmpty()): ?>
-                                    <div class='carousel-cell-image video-cell'>
-                                        <?php $vidURL = "https://www.youtube.com/watch?v=" . $album->video()->value(); ?>
+                                <span class='project__headline'><?= $album->headline()->value() ?></span>
+                                <p class='project__sub'>
+                                    <?= $album->subheading()->value() ?>
+                                </p>
+                                <p class='project__desc'>
+                                    <?= $album->description()->value() ?>
+                                </p>
+                            </div>
+                            <div class='project__images main-carousel lazy'>
+                                <?php if ($album->video()->isNotEmpty()): ?>
+                                <div class='carousel-cell-image video-cell'>
+                                    <?php $vidURL = "https://www.youtube.com/watch?v=" . $album->video()->value(); ?>
 
-                                        <?= youtube($vidURL) ?>
-                                            <!--
+                                    <?= youtube($vidURL) ?>
+                                    <!--
                                             <div class="video-cell__grippy"></div>
                                             <div class="video-cell__grippy"></div>
 -->
-                                    </div>
-                                    <?php endif ?>
-
-
-                                    <?php foreach($album->images() as $img): ?>
-                                    <img class='carousel-cell-image' data-flickity-lazyload-srcset='
-<?= $img->srcset([375, 800, 1024]) ?>' data-flickity-lazyload='<?= $img->url() ?>'>
-                                    <?php endforeach ?>
                                 </div>
+                                <?php endif ?>
+
+
+                                <?php foreach($album->images()->sortBy('sort') as $img): ?>
+                                <img class='carousel-cell-image' data-flickity-lazyload-srcset='
+<?= $img->srcset([375, 800, 1024]) ?>' data-flickity-lazyload='<?= $img->url() ?>'>
+                                <?php endforeach ?>
                             </div>
-                        </article>
-                    </div>
-                </li>
-                <?php endforeach ?>
-            </ul>
-        </section>
+                        </div>
+                    </article>
+                </div>
+            </li>
+            <?php endforeach ?>
+        </ul>
+    </section>
 
-        <section id="client-list" class="scroll-title">
+    <section id="client-list" class="scroll-title">
 
-            <span class="section__title">
-                CLIENTS <span> D.R </span> CLIENTS <span> D.R </span> CLIENTS <span> D.R </span> CLIENTS <span> D.R </span> CLIENTS <span> D.R </span> CLIENTS <span> D.R </span> CLIENTS <span> D.R </span> CLIENTS <span> D.R </span> CLIENTS <span> D.R </span> CLIENTS <span> D.R </span> CLIENTS
+        <span class="section__title">
+            CLIENTS <span> D.R </span> CLIENTS <span> D.R </span> CLIENTS <span> D.R </span> CLIENTS <span> D.R </span> CLIENTS <span> D.R </span> CLIENTS <span> D.R </span> CLIENTS <span> D.R </span> CLIENTS <span> D.R </span> CLIENTS <span> D.R </span> CLIENTS <span> D.R </span> CLIENTS
 
             <!--
             <?php foreach ($site->clients()->toStructure()->shuffle() as $clientlogo): ?> 
              CLIENTS <img src='<?php echo $clientlogo->logos()->toFile()->url() ?> '>
             <?php endforeach ?>
 -->
-            </span>
-            <article>
-                <?php
+        </span>
+        <article>
+            <?php
                 $tagsall = page('projects')->children()->listed()->pluck('tags', ',', true);
                 
                 count($tagsall)%2 !== 0 ? $numChunks = count($tagsall)/2 + .5 : $numChunks = count($tagsall)/2;
@@ -126,30 +126,30 @@
 
 
 
-                    <ul class="client__archive">
-                        <?php foreach($tagsall as $tag): ?>
-                        <li class="client__title">
-                            <a href="<?= url('projects', ['params' => ['tag' => urlencode($tag)]]) ?>">
-                                <?= html($tag) ?>
-                            </a>
-                        </li>
-                        <?php endforeach ?>
-                    </ul>
-            </article>
+            <ul class="client__archive">
+                <?php foreach($tagsall as $tag): ?>
+                <li class="client__title">
+                    <a href="<?= url('projects', ['params' => ['tag' => urlencode($tag)]]) ?>">
+                        <?= html($tag) ?>
+                    </a>
+                </li>
+                <?php endforeach ?>
+            </ul>
+        </article>
 
-        </section>
+    </section>
 
-        <section id="about-agency" class="scroll-title">
-            <span class="section__title">ABOUT <span> D.R </span> ABOUT <span> D.R </span> ABOUT <span> D.R </span> ABOUT <span> D.R </span> ABOUT <span> D.R </span> ABOUT <span> D.R </span> ABOUT <span> D.R </span> ABOUT <span> D.R </span> ABOUT <span> D.R </span> ABOUT <span> D.R </span> ABOUT </span>
-            <div class="about__dark-text">
-
-
-                <?= $site->agency()->kt() ?>
-            </div>
-
-        </section>
+    <section id="about-agency" class="scroll-title">
+        <span class="section__title">ABOUT <span> D.R </span> ABOUT <span> D.R </span> ABOUT <span> D.R </span> ABOUT <span> D.R </span> ABOUT <span> D.R </span> ABOUT <span> D.R </span> ABOUT <span> D.R </span> ABOUT <span> D.R </span> ABOUT <span> D.R </span> ABOUT <span> D.R </span> ABOUT </span>
+        <div class="about__dark-text">
 
 
+            <?= $site->agency()->kt() ?>
+        </div>
+
+    </section>
 
 
-        <?php snippet('footer') ?>
+
+
+    <?php snippet('footer') ?>
--- site/templates/projects.php
+++ site/templates/projects.php
@@ -9,32 +9,32 @@
  * More about templates: https://getkirby.com/docs/guide/templates/basics
  */
 ?>
-    <?php snippet('header') ?>
-    <a class="back" href="<?= $site->url() ?>#client-list"></a>
+<?php snippet('header') ?>
+<a class="back" href="<?= $site->url() ?>#client-list"></a>
 
-    <nav id="menu" class="menu">
-        <div class="address">
-            <!--<p>DAVID RODGERS INC</p>-->
-            <p>
-                <?= $site->address()->kirbytextinline() ?>
-            </p>
+<nav id="menu" class="menu">
+    <div class="address">
+        <!--<p>DAVID RODGERS INC</p>-->
+        <p>
+            <?= $site->address()->kirbytextinline() ?>
+        </p>
 
-        </div>
-        <div class="contact">
-            <p>T:
-                <?= $site->phone()->kirbytextinline() ?>
-            </p>
-            <p><a href="mailto:<?= $site->email() ?>"><?= $site->email()->kt() ?></a></p>
-        </div>
+    </div>
+    <div class="contact">
+        <p>T:
+            <?= $site->phone()->kirbytextinline() ?>
+        </p>
+        <p><a href="mailto:<?= $site->email() ?>"><?= $site->email()->kt() ?></a></p>
+    </div>
 
-    </nav>
-    </header>
-    <svg height="0">
+</nav>
+</header>
+<svg height="0">
 </svg>
 
-    <div class="main__content">
+<div class="main__content">
 
-        <?php 
+    <?php 
 $albums = page('projects')->children()->listed()->flip();
 if($tag = urldecode(param('tag'))){
     $albums = $albums->filterBy('tags', $tag, ',');
@@ -42,58 +42,58 @@ if($tag = urldecode(param('tag'))){
 //    echo "<script>console.log('" . json_encode($albums) . "');</script>";s
     ?>
 
-        <section class="proj featured">
-            <ul class="client" data-page="<?= $page->url() ?>">
-                <?php foreach ($albums as $album): ?>
-                <li class="client-name">
+    <section class="proj featured">
+        <ul class="client" data-page="<?= $page->url() ?>">
+            <?php foreach ($albums as $album): ?>
+            <li class="client-name">
 
-                    <div class="client-content">
-                        <article>
-                            <div class='project__container'>
-                                <div class='project__text'>
-                                    <h3 class="client__title">
+                <div class="client-content">
+                    <article>
+                        <div class='project__container'>
+                            <div class='project__text'>
+                                <h3 class="client__title">
 
-                                        <?php foreach ($album->tags()->split() as $tags): ?>
-                                        <span><?= $tags ?></span>
-                                        <?php endforeach ?>
-                                    </h3>
+                                    <?php foreach ($album->tags()->split() as $tags): ?>
+                                    <span><?= $tags ?></span>
+                                    <?php endforeach ?>
+                                </h3>
 
-                                    <span class='project__headline'><?= $album->headline()->value() ?></span>
-                                    <p class='project__sub'>
-                                        <?= $album->subheading()->value() ?>
-                                    </p>
-                                    <p class='project__desc'>
-                                        <?= $album->description()->value() ?>
-                                    </p>
-                                </div>
-                                <div class='project__images main-carousel lazy'>
-                                    <?php if ($album->video()->isNotEmpty()): ?>
-                                    <div class='carousel-cell-image video-cell'>
-                                        <?php $vidURL = "https://www.youtube.com/watch?v=" . $album->video()->value(); ?>
+                                <span class='project__headline'><?= $album->headline()->value() ?></span>
+                                <p class='project__sub'>
+                                    <?= $album->subheading()->value() ?>
+                                </p>
+                                <p class='project__desc'>
+                                    <?= $album->description()->value() ?>
+                                </p>
+                            </div>
+                            <div class='project__images main-carousel lazy'>
+                                <?php if ($album->video()->isNotEmpty()): ?>
+                                <div class='carousel-cell-image video-cell'>
+                                    <?php $vidURL = "https://www.youtube.com/watch?v=" . $album->video()->value(); ?>
 
-                                        <?= youtube($vidURL) ?>
-                                            <!--
+                                    <?= youtube($vidURL) ?>
+                                    <!--
                                             <div class="video-cell__grippy"></div>
                                             <div class="video-cell__grippy"></div>
 -->
-                                    </div>
-                                    <?php endif ?>
-
-
-
-                                    <?php foreach($album->images() as $img): ?>
-                                    <img class='carousel-cell-image' data-flickity-lazyload-srcset='
-<?= $img->srcset([375, 800, 1024]) ?>' data-flickity-lazyload='<?= $img->url() ?>'>
-                                    <?php endforeach ?>
                                 </div>
+                                <?php endif ?>
+
+
+
+                                <?php foreach($album->images()->sortBy('sort') as $img): ?>
+                                <img class='carousel-cell-image' data-flickity-lazyload-srcset='
+<?= $img->srcset([375, 800, 1024]) ?>' data-flickity-lazyload='<?= $img->url() ?>'>
+                                <?php endforeach ?>
                             </div>
-                        </article>
-                    </div>
-                </li>
-                <?php endforeach ?>
-            </ul>
-        </section>
+                        </div>
+                    </article>
+                </div>
+            </li>
+            <?php endforeach ?>
+        </ul>
+    </section>
 
 
 
-        <?php snippet('footer') ?>
+    <?php snippet('footer') ?>
